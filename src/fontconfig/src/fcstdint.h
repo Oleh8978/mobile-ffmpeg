b98fb6ed110a54854b211f8b56137e53b8afbba9
Oleg8978 <mihajlovskyoleg@gmail.com>
2026-02-08 16:30:16 +0100

    updated files

--- src/fontconfig/src/fcstdint.h
+++ src/fontconfig/src/fcstdint.h
@@ -2,7 +2,7 @@
 #define _FONTCONFIG_SRC_FCSTDINT_H 1
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H "fontconfig 2.13.92"
-/* generated using gnu compiler gcc (GCC) 9.1.1 20190605 (Red Hat 9.1.1-2) */
+/* generated using gnu compiler Android (9352603, based on r450784d1) clang version 14.0.7 (https://android.googlesource.com/toolchain/llvm-project 4c603efb0cca074e9238af8b4106c30add4418f6) */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
